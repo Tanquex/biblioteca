53d1f887c601f319446437644654968c81cdd9a2
Tanquex <114451376+Tanquex@users.noreply.github.com>
2024-11-28 09:42:46 -0600

    Update server.js

--- Servidor/server.js
+++ Servidor/server.js
@@ -15,25 +15,19 @@ const upload = multer({
   },
 });
 
-const corsOptions = [
-  'https://biblioteca-front-9gme.onrender.com',
-  'https://d2l31qkx2rm6ft.cloudfront.net'];
+// const corsOptions = [
+//   'https://biblioteca-front-9gme.onrender.com',
+//   'https://d2l31qkx2rm6ft.cloudfront.net'];
 
-// const corsOptions = {
-//   origin: 'https://biblioteca-front-9gme.onrender.com', 'https://d2l31qkx2rm6ft.cloudfront.net',// Reemplaza con tu dominio de CloudFront
-//   optionsSuccessStatus: 200
-// };
+const corsOptions = {
+  origin: 'https://d2l31qkx2rm6ft.cloudfront.net',// Reemplaza con tu dominio de CloudFront
+  optionsSuccessStatus: 200
+};
 
 app.use(cors({
-  origin: function (origin, callback) {
-  // Permitir solicitudes sin origen (como Postman)
-  if (!origin) return callback(null,true);
-  if (allowedOrigins.indexof(origin) ===-1){
-  const msg = 'El origen ' + origin +' no está permitido';
-  return callback(new Error(msg), false);
-  }
-  return callback(null, true);
-  }
+  origin: 'https://d2l31qkx2rm6ft.cloudfront.net',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(bodyParser.json());
